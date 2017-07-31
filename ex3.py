b3c7da79d0c7b717487e410f81e13d93f91bf4b9
perfectfoolish <DZKXperfect@gmail.com>
2017-07-31 21:37:18 +0800

    Numbers and mathematical calculation

---
+++ ex3.py
@@ -0,0 +1,36 @@
+# Print some words
+print "I will now count my chickens:"
+
+# Calculate Addition, Division
+print "Hens", 25 + 30 / 6
+# Calculate Subtraction, Multiplication, mod
+print "Roosters", 100 - 25*3%4
+
+# Print some words
+print "Now I will count the eggs:"
+
+# Why 1/4 equal to 0 ? Now I know the reason.
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4.0 + 6
+
+# Print some words
+print "Is it true that 3 + 2 < 5 - 7 ?"
+
+# less operator
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2 ?", 3 + 2
+print "What is 5 - 7 ?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+# greater operator
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+
+print 7.0 / 4.0
+print 7 / 4
