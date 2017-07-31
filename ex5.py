5962c606c8161ba6ce72e61f9399ad27e5eb3cb5
perfectfoolish <DZKXperfect@gmail.com>
2017-08-01 07:52:26 +0800

    Additional exercises

--- ex5.py
+++ ex5.py
@@ -1,17 +1,26 @@
-my_name = 'Zed A.Shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A.Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
 print "Actually that's not to heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+print "If I add %d, %d, and %d I get %d." % (age, height, weight, age + height + weight)
+
+print "He's %r inches tall and got %r eyes." % (height, eyes)
+
+print "His height is %f m" % 1.83
+print "His height is %.2f m" % 1.83
+
+print "2 inches equal to %.2f cm" % (2 * 2.54)
+print "1 lb equal to %.7f kg" % (1 * 0.4535924)
+print "2 lb equal to %.7f kg" % round(2 * 0.4535924)
