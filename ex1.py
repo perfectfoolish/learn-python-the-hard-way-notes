2895d8d9a995c82c24739c5357b048f2a73b1f70
perfectfoolish <DZKXperfect@gmail.com>
2017-07-31 19:46:21 +0800

    Hello world exercise, use print.

---
+++ ex1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+print "利用好这几个月的时间"
+print "Hello World!"
+print "Hello Again."
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+#print "This time I will master programming, I come, I see, ......"
+print "\n"
