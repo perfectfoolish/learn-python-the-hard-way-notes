95c62e17c979428486801eb0e0914639015f6256
perfectfoolish <DZKXperfect@gmail.com>
2017-08-01 15:32:33 +0800

    argument, unpack, variable

---
+++ ex13-1.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script_name, first, second, third, fourth = argv
+
+encourage_words = raw_input("Please write some words encourage yourself.")
+
+print "Script name:", script_name
+print "First variable:", first
+print "Second variable:", second
+print "Third variable:", third
+print "Fourth variable:", fourth
+
+print "You tell yourself:", encourage_words
