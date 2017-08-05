f10b01e76b8887a1a59570fd2663c32d8b1ef4ac
perfectfoolish <DZKXperfect@gmail.com>
2017-08-05 19:58:55 +0800

    For loop

--- ex33.py
+++ ex33.py
@@ -2,7 +2,6 @@ numbers = []
 
 def build_list(length, step):
     i = 0
-
     while i < length:
         print "At the top i is %d" % i
         numbers.append(i)
@@ -11,7 +10,16 @@ def build_list(length, step):
         print "Numbers now:", numbers
         print "At the bottom i is %d" % i
 
-build_list(5, 2)
+for i in range(1, 6):
+
+    print "At the top i is %d" % i
+    numbers.append(i)
+
+    i = i + 1
+    print "Numbers now:", numbers
+    print "At the bottom i is %d" % i
+
+#build_list(5, 2)
 print "The numbers:"
 
 for num in numbers:
