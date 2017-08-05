9f4fd75fe2367c1f447c2ff129ffb02c07426db6
perfectfoolish <DZKXperfect@gmail.com>
2017-08-05 10:26:08 +0800

    While loop of function version

---
+++ ex33.py
@@ -0,0 +1,18 @@
+numbers = []
+
+def build_list(length, step):
+    i = 0
+
+    while i < length:
+        print "At the top i is %d" % i
+        numbers.append(i)
+
+        i = i + step
+        print "Numbers now:", numbers
+        print "At the bottom i is %d" % i
+
+build_list(5, 2)
+print "The numbers:"
+
+for num in numbers:
+    print num
